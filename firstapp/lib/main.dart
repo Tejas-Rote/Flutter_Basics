d5a93c4b6df363ef3d134ffa47ebb246dd765b5e
Tejas Rote <tejas292002@gmail.com>
2021-11-25 19:23:50 +0530

    video-7

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,38 +1,47 @@
 import 'package:flutter/material.dart';
 
-// video - 4 
+// video - 4
 // void main() => runApp(MaterialApp(
 //   home: Text('Hello world ')
 // ));
 
-// video -5
+// video -5&6
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Home'),
-      centerTitle: true,
-      backgroundColor:Colors.red[400] ,
-    ),
-    body:Center( //wrap using center widget to centre anything
-      child: Text(
-        'Hello',
-        style: TextStyle(
-          fontSize: 40,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 3,
-          color: Colors.indigo[30],
-          fontFamily: 'Inconsolata',
-        ) 
-        ),
-       //use child proprty while nesting
-    ), 
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('Click'),
-      backgroundColor: Colors.green,
-    ),
-  )
-));
+      home: Home(),
+    ));
 
 
 
+// video 7
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
+      ),
+      body: Center(
+        //wrap using center widget to centre anything
+        child: Text('Hello',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 3,
+              color: Colors.indigo[30],
+              fontFamily: 'Inconsolata',
+            )),
+        //use child proprty while nesting
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
